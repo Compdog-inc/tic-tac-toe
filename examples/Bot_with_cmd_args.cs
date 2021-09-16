0e9b14a50b159660c9361da13652d234fa33d0e0
Compdog Inc <66779418+Compdog-inc@users.noreply.github.com>
2021-09-16 18:40:03 +0300

    Create Bot_with_cmd_args.cs

---
+++ examples/Bot_with_cmd_args.cs
@@ -0,0 +1,36 @@
+using System;
+using Compdog.TicTacToe.Utils;
+
+namespace Compdog.TicTacToe.Bots.ExampleBot
+{
+    public class Bot : IBot
+    {
+        public string ShortDescription => "Example bot";
+        public string Description => "A detailed description of what your bot can do.";
+        
+        private Arguments args;
+        
+        public bool Load(Loader loader, Arguments args)
+        {
+            this.args = args;
+            return true;    // Return false to not load this bot
+        }
+        
+        public bool Unload(Loader loader)
+        {
+            return true;    // Return false to show warning
+        }
+        
+        public bool Event(Loader loader, SubscribeEventArgs e)
+        {
+            return false;   // Return true to terminate
+        }
+        
+        public bool Run(Game game, string[] args)
+        {
+            Console.WriteLine(args.ToString());     // Print all cmd args
+            Console.WriteLine("This is an example.");
+            return true;    // Return status (true - ok, false - failed)
+        }
+    }
+}
