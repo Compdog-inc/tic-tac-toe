400d39abdbcd2177a24e7211648289fa514fe4c4
Compdog Inc <66779418+Compdog-inc@users.noreply.github.com>
2021-09-16 18:29:35 +0300

    Update Bot.cs

--- Bot.cs
+++ Bot.cs
@@ -1,4 +1,5 @@
 using System;
+using Compdog.TicTacToe.Utils;
 
 namespace Compdog.TicTacToe.Bots.ExampleBot
 {
@@ -6,6 +7,21 @@ namespace Compdog.TicTacToe.Bots.ExampleBot
     {
         public string ShortDescription => "Example bot";
         public string Description => "A detailed description of what your bot can do.";
+        
+        public bool Load(Loader loader, Arguments args)
+        {
+            return true;    // Return false to not load this bot
+        }
+        
+        public bool Unload(Loader loader)
+        {
+            return true;    // Return false to show warning
+        }
+        
+        public bool Event(Loader loader, SubscribeEventArgs e)
+        {
+            return false;   // Return true to terminate
+        }
         
         public bool Run(Game game, string[] args)
         {
