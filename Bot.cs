9adf057a2c47d8c5976653eb37e2d6b88f818ebe
Compdog Inc <66779418+Compdog-inc@users.noreply.github.com>
2021-09-16 14:50:46 +0300

    Update Bot.cs

--- Bot.cs
+++ Bot.cs
@@ -1,1 +1,16 @@
-//TODO: make example code
+using System;
+
+namespace Compdog.TicTacToe.Bots.ExampleBot
+{
+    public class Bot : IBot
+    {
+        public string ShortDescription => "Example bot";
+        public string Description => "A detailed description of what your bot can do.";
+        
+        public bool Run(Game game, string[] args)
+        {
+            Console.WriteLine("This is an example.");
+            return true;    // Return status (true - ok, false - failed)
+        }
+    }
+}
